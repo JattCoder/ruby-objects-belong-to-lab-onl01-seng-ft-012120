4f8ccd27343b74be77e39f928989decaccf72a1f
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-02-02 03:50:37 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -10,28 +10,5 @@ class Post
 
 end
 
-class Author
-
-  def author=(author)
-    @author = author
-  end
-
-  def author
-    @author
-  end
-
-  def name=(name)
-    @name = name
-  end
-
-  def name
-    @name
-  end
-
-end
-
 post = Post.new
 post.title
-author = Author.new
-author.author
-author.author.name
